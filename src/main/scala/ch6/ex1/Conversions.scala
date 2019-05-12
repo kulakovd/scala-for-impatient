8614f4bc73ebac5a7cf25ee8b27902bf39c7d545
Dmitry Kulakov <dmitry.kulakov.00@gmail.com>
2019-05-12 19:54:05 +0500

    add exercise 1, chapter 6

---
+++ src/main/scala/ch6/ex1/Conversions.scala
@@ -0,0 +1,7 @@
+package ch6.ex1
+
+object Conversions {
+	def inchesToCentimeters(inches: Double): Double = inches * 2.54
+	def gallonsToLiters(gallons: Double): Double = gallons * 3.785
+	def milesToKilometers(miles: Double): Double = miles * 1.609
+}
