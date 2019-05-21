6a0d8cc19913400198d804851643d1ae3c6d54b0
Dmitry Kulakov <dmitry.kulakov.00@gmail.com>
2019-05-22 00:26:20 +0500

    add exercise 5, chapter 8

---
+++ src/main/scala/ch8/ex5/LabeledPoint.scala
@@ -0,0 +1,5 @@
+package ch8.ex5
+
+class LabeledPoint(private val label: String, x: Double, y: Double) extends Point(x, y) {
+
+}
---
+++ src/main/scala/ch8/ex5/Point.scala
@@ -0,0 +1,5 @@
+package ch8.ex5
+
+class Point(val x: Double, val y: Double) {
+
+}
