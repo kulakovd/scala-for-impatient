e70d05fe8b57eb3fb466384bcee3f0dbec21c2c3
Dmitry Kulakov <dmitry.kulakov.00@gmail.com>
2019-05-22 02:24:50 +0500

    fix exercise 7, chapter 8

--- src/main/scala/ch8/ex7/Square.scala
+++ src/main/scala/ch8/ex7/Square.scala
@@ -2,7 +2,7 @@ package ch8.ex7
 
 import java.awt.Rectangle
 
-class Square(val x: Int, val y: Int, val width: Int) extends Rectangle(x, y, width, width) {
+class Square(x: Int, y: Int, width: Int) extends Rectangle(x, y, width, width) {
 
 	def this(width: Int) {
 		this(0, 0, width)
